a1a86385533b131f9adb1ddc617c180efc36ff69
tuhin92 <tuhinalmamun71m@gmail.com>
2024-02-13 23:14:39 +0600

    added Parallelogram area

---
+++ script/parallelogram-calculate.js
@@ -0,0 +1,21 @@
+function calculateParallelogramArea(){
+    // get base of parallelogram 
+    const parallelogramBaseInput = document.getElementById('parallelogram-base');
+    const parallelogramBaseText = parallelogramBaseInput.value;
+    const base = parseFloat(parallelogramBaseText)
+    console.log(base);
+
+    // get height parallelogram 
+    const parallelogramHeightInput = document.getElementById('parallelogram-height');
+    const parallelogramHeightText = parallelogramHeightInput.value;
+    const height = parseFloat(parallelogramHeightText)
+    console.log(height);
+
+    // Calculate Parallelogram Area 
+    const area = base * height;
+    console.log('Area of parallelogram is: ', area);
+
+    // Calculate Parallelogram Area 
+    const parallelogramAreaDisplay = document.getElementById('parallelogram-area');
+    parallelogramAreaDisplay.innerText = area;
+}
