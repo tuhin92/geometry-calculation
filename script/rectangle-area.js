65b3595c5a2471c4018419abf2e67a63109967c5
tuhin92 <tuhinalmamun71m@gmail.com>
2024-02-13 22:53:27 +0600

    added rectangle area

--- script/rectangle-area.js
+++ script/rectangle-area.js
@@ -1,3 +1,19 @@
 function calculateRectangleArea(){
-    console.log('gg');
+    // get width of the rectangle 
+    const rectangleWidthInput = document.getElementById('rectangle-width')
+    const rectangleWidthText = rectangleWidthInput.value;
+    const width = parseFloat(rectangleWidthText)
+     
+
+    // get length of the rectangle 
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLengthText)
+
+    // Calculate Rectangle Area 
+    const area = width * length;
+    console.log('Area of rectangle is: ', area);
+
+    const rectangleAreaDisplay = document.getElementById('rectangle-area');
+    rectangleAreaDisplay.innerText = area;
 }
